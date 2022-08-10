c2a1c4f56a61ac43d6391b23ff186325f8d0680f
ntwrkjasen <jasen@thentwrk.com>
2022-08-10 06:27:16 -0500

    icap banner styles

--- template-parts/icap.php
+++ template-parts/icap.php
@@ -23,41 +23,45 @@
         </div>
         <div class="bg-gsap-blue flex flex-col justify-center w-1/2">
             <div class="max-w-2xl mx-auto">
-                <div class="text-center">
+                <div class="text-center font-magdelinBold mb-6">
                     <?php echo $icap_banner_content_header; ?>
                 </div>
                 <div class="columns flex">
                     <?php foreach($icap_banner_content_columns as $item): $column_image = $item['column']['image']; $column_image_list = $item['column']['list']; $column_arrow = $item['column']['arrow'] ?>
-                        <div class="column flex-none w-1/2 mr-16 last:mr-0">
+                        <div class="column flex-none px-4 w-1/2">
                             <figure class="mb-6">
-                                <img class="h-40" src="<?php echo $column_image['url']; ?>" alt="<?php echo $column_image['alt']; ?>" />
+                                <img class="h-32 mx-auto" src="<?php echo $column_image['url']; ?>" alt="<?php echo $column_image['alt']; ?>" />
                             </figure>
                             <div class="text-center">
                                 <?php foreach($column_image_list as $list): $list_item = $list['list_item'] ?>
-                                    <div class="border-t border-b border-white last:border-b-0">
+                                    <div class="border-t border-white last:border-b-0 py-4 last:pb-0">
                                         <?php echo $list_item; ?>
                                     </div>
                                 <?php endforeach; ?>
                             </div>
-                            <figure>
-                                <img class="w-16" src="<?php echo $column_arrow['url']; ?>" alt="<?php echo $column_arrow['alt']; ?>" />
+                            <figure class="mt-3">
+                                <img class="w-16 mx-auto" src="<?php echo $column_arrow['url']; ?>" alt="<?php echo $column_arrow['alt']; ?>" />
                             </figure>
                         </div>
                     <?php endforeach; ?>
                 </div>
-                <div>
-                    <div>
-                        <div class="flex">
-                            <figure>
-                                <img class="w-8" src="<?php echo $icap_banner_content_footer_header_image['url']; ?>" alt="<?php echo $icap_banner_content_footer_header_image['alt']; ?>" />
+                <div class="mt-6">
+                    <div class="mb-8">
+                        <div class="w-60 mx-auto">
+                            <div class="flex items-center">
+                                <figure class="mr-3">
+                                    <img class="w-8" src="<?php echo $icap_banner_content_footer_header_image['url']; ?>" alt="<?php echo $icap_banner_content_footer_header_image['alt']; ?>" />
+                                </figure>
+                                <h4 class="uppercase tracking-[.2rem] mb-0 text-[1.50rem] flex-grow"><?php echo $icap_banner_content_footer_header_text; ?></h4>
+                            </div>
+                            <figure class="mt-2">
+                                <img class="w-full" src="<?php echo $icap_banner_content_footer_header_underline['url'];?>" alt="<?php echo $icap_banner_content_footer_header_underline['alt'];?>" />
                             </figure>
-                            <h4 class="uppercase"><?php echo $icap_banner_content_footer_header_text; ?></h4>
                         </div>
                     </div>
-                    <figure>
-                        <img class="w-full" src="<?php echo $icap_banner_content_footer_header_underline['url'];?>" alt="<?php echo $icap_banner_content_footer_header_underline['alt'];?>" />
-                    </figure>
-                    <?php echo $icap_banner_content_footer_header_content; ?>
+                    <div class="text-center max-w-lg mx-auto">
+                        <?php echo $icap_banner_content_footer_header_content; ?>
+                    </div>
                 </div>
             </div>
         </div>
