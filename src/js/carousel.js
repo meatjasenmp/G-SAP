782180ff53f80e6989a6b94c0374c972ed6c34f8
ntwrkjasen <jasen@thentwrk.com>
2022-08-11 06:22:31 -0500

    carousel init

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,3 +1,27 @@
-const carousel = () => {};
+import Flickity from "flickity";
+
+const carousel = () => {
+  const element = document.querySelector(".carousel");
+  if (!element) return;
+  const flkty = new Flickity(element, {
+    imagesLoaded: true,
+    initialIndex: 1,
+  });
+
+  Flickity.prototype._createResizeClass = function () {
+    this.element.classList.add("flickity-resize");
+  };
+
+  Flickity.createMethods.push("_createResizeClass");
+
+  var resize = Flickity.prototype.resize;
+  Flickity.prototype.resize = function () {
+    this.element.classList.remove("flickity-resize");
+    resize.call(this);
+    this.element.classList.add("flickity-resize");
+  };
+
+  return flkty;
+};
 
 export default carousel;
--- template-parts/resources.php
+++ template-parts/resources.php
@@ -8,28 +8,32 @@
     $resources_carousel_cells = $resources_carousel['cells'];
 ?>
 
-<section class="resources bg-gsap-blue text-white py-16 relative w-screen left-2/4 right-2/4 ml-[-50vw] mr-[-50vw]">
+<section class="resources bg-gsap-blue text-white py-20 relative w-screen left-2/4 right-2/4 ml-[-50vw] mr-[-50vw]">
     <div class="max-w-[1440px] mx-auto">
         <div class="max-w-xl mx-auto text-center">
             <h5 class="uppercase tracking-widest text-sm"><?php echo $resources_sub_header; ?></h5>
             <h1 class="mb-3"><?php echo $resources_header_text; ?></h1>
             <?php echo $resources_header_content; ?>
         </div>
-        <div class="carousel">
+        <div class="carousel mt-20 max-w-4xl mx-auto">
             <?php foreach($resources_carousel_cells as $cell): ?>
-                <?php foreach($cell as $cell_item): $image = $cell_item['image']; $header = $cell_item['text']; $content = $cell_item['content']; $footer = $cell_item['footer']; ?>
-                    <div class="cell">
-                        <figure>
-                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                        </figure>
-                        <div>
-                            <h5><?php echo $header; ?></h5>
-                            <?php echo $content; ?>
-                            <div>
-                                <figure>
-                                    <img src="<?php echo $footer['image']['url']; ?>" alt="<?php echo $footer['image']['alt']; ?>" />
-                                </figure>
-                                <span><?php echo $footer['text']; ?></span>
+                <?php foreach($cell as $cell_item): $image = $cell_item['image']; $sub_header = $cell_item['sub_header']; $content = $cell_item['content']; $footer = $cell_item['footer']; ?>
+                    <div class="carousel-cell">
+                        <div class="cell-wrap">
+                            <figure class="hero">
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            </figure>
+                            <div class="max-w-xs mx-auto flex flex-col items-center text-center">
+                                <div>
+                                    <h4 class="mb-0"><?php echo $sub_header; ?></h4>
+                                    <?php echo $content; ?>
+                                </div>
+                                <div class="flex items-center">
+                                    <figure class="mr-1 w-3">
+                                        <img src="<?php echo $footer['image']['url']; ?>" alt="<?php echo $footer['image']['alt']; ?>" />
+                                    </figure>
+                                    <span class="text-xs uppercase font-magdelinBold block"><?php echo $footer['text']; ?></span>
+                                </div>
                             </div>
                         </div>
                     </div>
