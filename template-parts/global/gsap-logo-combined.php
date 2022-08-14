9b40a1ee8c35c018e1c9d46e8aac4dd3f4fb25e9
ntwrkjasen <jasen@thentwrk.com>
2022-08-14 07:40:03 -0500

    Init responsiveness.

--- template-parts/global/gsap-logo-combined.php
+++ template-parts/global/gsap-logo-combined.php
@@ -1,8 +1,8 @@
-<div class="gsap_logo_combined flex w-[10rem]">
+<div class="gsap_logo_combined flex w-[14rem] lg:w-[10rem]">
     <div class="relative z-10">
         <?php get_template_part('template-parts/global/gsap-logo'); ?>
     </div>
-    <div class="relative -ml-[9rem]">
+    <div class="relative -ml-[6rem] lg:-ml-[9rem]">
         <?php get_template_part('template-parts/global/life-circle'); ?>
     </div>
 </div>
