9222ece60bfdd4ce62b53a9c0386a733d104d62b
ntwrkjasen <jasen@thentwrk.com>
2022-08-23 19:07:57 -0500

    Extracting pulsating button CSS.

--- template-parts/global/pulsating-button.php
+++ template-parts/global/pulsating-button.php
@@ -1,10 +1,10 @@
 <div class="pulsating__button__container">
-    <div class="bg border-gsap-orange border absolute w-[35px] h-[35px] rounded-full left-0 right-0 top-[-1.25rem] mx-auto"></div>
-    <div class="bg border-gsap-orange border absolute w-[35px] h-[35px] rounded-full left-0 right-0 top-[-1.25rem] mx-auto"></div>
-    <button class="pulsating__button flex justify-center items-center absolute z-50 bg-gsap-orange outline-0 w-[35px] h-[35px] rounded-full left-0 right-0 mx-auto top-[-1.25rem]">
-        <div class="plus_button relative w-[35px] h-[35px]">
-            <div class="plus_button__line plus__button__line-horizontal bg-white absolute w-[20px] h-[.20rem] top-0 bottom-0 left-0 right-0 m-auto"></div>
-            <div class="plus_button__line plus__button__line-vertical bg-white absolute h-[20px] w-[3px] hidden"></div>
+    <div class="bg"></div>
+    <div class="bg"></div>
+    <button class="pulsating__button">
+        <div class="plus_button">
+            <div class="plus_button__line plus__button__line-horizontal"></div>
+            <div class="plus_button__line plus__button__line-vertical"></div>
         </div>
     </button>
 </div>
