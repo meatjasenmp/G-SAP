b31fa133e19270f6259a6a8cbb7d09a1e9484cba
ntwrkjasen <jasen@thentwrk.com>
2022-08-24 06:06:12 -0500

    Extracting how gsap works CSS.

--- template-parts/how-does-gsap-work.php
+++ template-parts/how-does-gsap-work.php
@@ -23,20 +23,20 @@
 ?>
 
 <section class="how">
-    <h1 class="text-gsap-blue"><?php echo $content_header; ?></h1>
-    <figure class="w-8">
+    <h1><?php echo $content_header; ?></h1>
+    <figure class="down_arrow">
         <img src="<?php echo $arrow['url']; ?>" alt="<?php echo $arrow['alt']; ?>">
     </figure>
-    <div class="columns flex justify-between mt-8">
-        <div class="column flex-none w-72">
+    <div class="columns">
+        <div class="column">
             <?php echo $content_content; ?>
         </div>
-        <div class="column flex">
+        <div class="column">
             <?php foreach($columns as $column):  ?>
                 <?php foreach($column as $item): $image = $item['image']; $content = $item['content'] ?>
-                    <div class="column flex-none w-72 mr-8 last:mr-0">
-                        <figure class="mb-2">
-                            <img class="h-[22px]" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                    <div class="column">
+                        <figure>
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
                         </figure>
                         <?php echo $content; ?>
                     </div>
@@ -47,40 +47,40 @@
     <div class="hero_image">
         <img src="<?php echo $desktop_image['url']; ?>" alt="<?php echo $desktop_image['alt']; ?>">
     </div>
-    <div class="earned_results flex items-center max-w-screen-lg mx-auto">
-        <div class="bg-gsap-blue text-white flex-none w-[600px] rounded-md p-8 mr-8">
-            <div class="flex">
-                <div class="mr-6">
-                    <figure class="w-12 mb-4">
+    <div class="earned_results">
+        <div class="container">
+            <div class="wrapper">
+                <div class="column_1">
+                    <figure>
                         <img src="<?php echo $earned_results_column_image['url']; ?>" alt="<?php echo $earned_results_column_image['alt']; ?>">
                     </figure>
-                    <h1 class="uppercase"><?php echo $earned_results_column_title; ?></h1>
+                    <h1><?php echo $earned_results_column_title; ?></h1>
                 </div>
-                <div>
-                    <div class="flex justify-between">
+                <div class="column_2">
+                    <div class="column_2_wrapper">
                         <?php $i = 0; foreach($earned_results_list as $list): ?>
                             <?php foreach($list as $item ): $checkbox = $item['checkbox']; $text = $item['text']; ?>
                                 <?php echo ($i % 2 === 0) ? '<div>' : '';  $i++; ?>
-                                    <div class="flex items-center mb-3 last:mb-0">
-                                        <img class="mr-2 w-4" src="<?php echo $checkbox['url']; ?>" alt="<?php echo $checkbox['alt']; ?>">
-                                        <span class="text-sm font-magdelinBold"><?php echo $text; ?></span>
+                                    <div class="column_2_item">
+                                        <img src="<?php echo $checkbox['url']; ?>" alt="<?php echo $checkbox['alt']; ?>">
+                                        <span><?php echo $text; ?></span>
                                     </div>
                                 <?php echo ($i % 2 === 0) ? '</div>' : ''?>
                             <?php endforeach; ?>
                         <?php endforeach; ?>
                     </div>
-                    <div class="border-t-2 border-white pt-2 mt-6 text-sm">
+                    <div class="column_2_text">
                         <?php echo strip_tags($earned_results_content_column_content, '<strong><em>'); ?>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="text-gsap-blue">
-            <header class="flex items-center mb-2">
-                <img class="mr-1" src="<?php echo $student_investment_of_time_header_image['url']; ?>" alt="<?php echo $student_investment_of_time_header_image['alt']; ?>">
-                <span class="uppercase text-xs"><?php echo $student_invest_of_time_header_text; ?></span>
+        <div class="student_invest_of_time">
+            <header>
+                <img src="<?php echo $student_investment_of_time_header_image['url']; ?>" alt="<?php echo $student_investment_of_time_header_image['alt']; ?>">
+                <span><?php echo $student_invest_of_time_header_text; ?></span>
             </header>
-            <?php echo str_replace('<p>', '<p class="text-sm last:mb-0">', $student_investment_of_time_content); ?>
+            <?php echo $student_investment_of_time_content; ?>
         </div>
     </div>
 </section>
