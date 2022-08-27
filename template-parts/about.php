9f3c17d1f88741cbf4980c173380d309bfc4613e
ntwrkjasen <jasen@thentwrk.com>
2022-08-27 13:41:43 -0500

    Responsive styles.

--- template-parts/about.php
+++ template-parts/about.php
@@ -1,6 +1,7 @@
 <?php
     $about = get_group_field('content_blocks', 'about', 29);
     $biography = $about['biography'];
+    $biography_arrow = $biography['arrow'];
     $biography_image = $biography['image'];
     $biography_image_image = $biography_image['image'];
     $biography_image_title = $biography_image['title'];
@@ -17,32 +18,40 @@
     $about_quote_image = $about_quote['image'];
 ?>
 
-<section class="about mt-20">
-    <div class="flex justify-between items-center max-w-7xl mx-auto">
-        <div>
-            <figure class="relative">
-                <div class="absolute top-12 left-12">
-                    <span class="block font-magdelinBold"><?php echo $biography_image_title_name; ?></span>
-                    <span class="block"><?php echo $biography_image_title_title; ?></span>
+<section class="about">
+    <div class="wrapper">
+        <div class="portrait_image_title portrait_image_title_mobile">
+            <h5 class="text-gsap-orange tracking-wider"><?php echo $biography_content_sub_header; ?></h5>
+            <span class="biography_image_name"><?php echo $biography_image_title_name; ?></span>
+            <span class="biography_image_title"><?php echo $biography_image_title_title; ?></span>
+        </div>
+        <div class="portrait_image_container">
+            <figure class="portrait_image_wrapper">
+                <div class="portrait_image_title">
+                    <span class="biography_image_name"><?php echo $biography_image_title_name; ?></span>
+                    <span class="biography_image_title"><?php echo $biography_image_title_title; ?></span>
                 </div>
-                <img class="rounded-full max-w-2xl" src="<?php echo $biography_image_image['url']; ?>" alt="<?php echo $biography_image_image['alt']; ?>">
-                <figure class="absolute bottom-[3.5rem] right-0 z-10">
+                <img class="portrait_image" src="<?php echo $biography_image_image['url']; ?>" alt="<?php echo $biography_image_image['alt']; ?>">
+                <figure class="life_circle">
                     <?php get_template_part('template-parts/global/life-circle'); ?>
                 </figure>
             </figure>
         </div>
-        <div class="flex-none max-w-[400px]">
+        <div class="biography_wrapper">
             <h5 class="text-gsap-orange tracking-wider"><?php echo $biography_content_sub_header; ?></h5>
             <h2><?php echo $biography_content_header; ?></h2>
             <?php echo $biography_content_content; ?>
+            <figure class="arrow">
+                <img src="<?php echo $biography_arrow['url']; ?>" alt="<?php echo $biography_arrow['alt']; ?>">
+            </figure>
         </div>
     </div>
-    <div class="max-w-xl mx-auto text-center mt-20">
-        <h5 class="uppercase text-gsap-blue tracking-wider mb-1 text-sm"><?php echo $about_mission_statement_subheader; ?></h5>
-        <p class="text-[1.15rem]"><?php echo strip_tags($about_mission_statement_content, ''); ?></p>
+    <div class="mission_statement">
+        <h5><?php echo $about_mission_statement_subheader; ?></h5>
+        <p><?php echo strip_tags($about_mission_statement_content, ''); ?></p>
     </div>
-    <div class="mt-20">
-        <figure class="max-w-2xl mx-auto">
+    <div class="quote_image">
+        <figure>
             <img src="<?php echo $about_quote_image['url'] ?>" alt="<?php echo $about_quote_image['alt'] ?>">
         </figure>
     </div>
