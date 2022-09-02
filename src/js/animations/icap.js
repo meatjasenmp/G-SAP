e65d0a81a05a993d690d9065f0a6a31278b7b85c
ntwrkjasen <jasen@thentwrk.com>
2022-09-02 11:25:28 -0500

    Transition tweaks

--- src/js/animations/icap.js
+++ src/js/animations/icap.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger);
 const icapTimeline = gsap.timeline();
 
 const icapHeroImageAnimation = () => {
-  icapTimeline.fromTo(
+  gsap.fromTo(
     ".icap .image_container",
     { opacity: 0, y: 50 },
     { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" }
@@ -14,62 +14,65 @@ const icapHeroImageAnimation = () => {
 };
 
 const icapHeroContentAnimation = () => {
-  icapTimeline.fromTo(
+  const icapHeroContentAnimationTimeline = gsap.timeline();
+  icapHeroContentAnimationTimeline.fromTo(
     ".icap .icap_banner_content_header",
+    { opacity: 0, y: 50 },
+    { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" }
+  );
+  icapHeroContentAnimationTimeline.fromTo(
+    ".icap .columns .column",
     { opacity: 0, y: 50 },
     { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
     "-=1.25"
   );
-  icapTimeline.fromTo(
-    ".icap .columns .column",
-    { opacity: 0, y: 50 },
-    { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
-    "-=1.15"
-  );
-  icapTimeline.fromTo(
+};
+
+const icapGraduationAnimation = () => {
+  const icapGraduationTimeline = gsap.timeline();
+  icapGraduationTimeline.fromTo(
     ".icap .graduation_graphic_text",
     { opacity: 0, y: 50 },
-    { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
-    "-=1.15"
+    { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" }
   );
-  icapTimeline.fromTo(
+  icapGraduationTimeline.fromTo(
     ".icap .graduation_graphic_underline",
     { opacity: 0, y: 50 },
     { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
-    "-=1.15"
+    "-=1.25"
   );
-  icapTimeline.fromTo(
+  icapGraduationTimeline.fromTo(
     ".icap .icap_banner_content_footer_header_content",
     { opacity: 0, y: 50 },
     { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
-    "-=1.15"
+    "-=1.25"
   );
 };
 
 const icapColumnAnimation = () => {
-  icapTimeline.fromTo(
+  const icapColumnAnimationTimeline = gsap.timeline();
+  icapColumnAnimationTimeline.fromTo(
     ".icap .learn_more_columns .sub_header",
+    { opacity: 0, y: 50 },
+    { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" }
+  );
+  icapColumnAnimationTimeline.fromTo(
+    ".icap .learn_more_columns .column span",
     { opacity: 0, y: 50 },
     { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
     "-=1.25"
   );
-  icapTimeline.fromTo(
-    ".icap .learn_more_columns .column span",
-    { opacity: 0, y: 50 },
-    { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
-    "-=1.15"
-  );
-  icapTimeline.fromTo(
+  icapColumnAnimationTimeline.fromTo(
     ".icap .learn_more_columns .column h4",
     { opacity: 0, y: 50 },
     { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
-    "-=1.15"
+    "-=1.25"
   );
-  icapTimeline.fromTo(
+  icapColumnAnimationTimeline.fromTo(
     ".icap .learn_more_columns .column article",
     { opacity: 0, y: 50 },
     { opacity: 1, y: 0, duration: 1.5, ease: "power4.out" },
-    "-=1.15"
+    "-=1.25"
   );
 };
 
@@ -78,7 +81,23 @@ const iCapScrollTrigger = () => {
     trigger: ".icap",
     onEnter: () => {
       icapHeroImageAnimation();
+    },
+  });
+  ScrollTrigger.create({
+    trigger: ".icap .icap_columns",
+    onEnter: () => {
       icapHeroContentAnimation();
+    },
+  });
+  ScrollTrigger.create({
+    trigger: ".icap .graduation",
+    onEnter: () => {
+      icapGraduationAnimation();
+    },
+  });
+  ScrollTrigger.create({
+    trigger: ".icap .learn_more_columns",
+    onEnter: () => {
       icapColumnAnimation();
     },
   });
