04d9ea769929a3c246d5a0f22245d48d6de5eb6a
ntwrkjasen <jasen@thentwrk.com>
2022-09-02 12:59:32 -0500

    Secondary nav init

--- template-parts/navigation/secondary.php
+++ template-parts/navigation/secondary.php
@@ -1,50 +1,30 @@
 <section class="secondary_nav">
-    <div class="home_nav">
-        <a href="">
-            <div class="badge">
-                Home
-            </div>
-            <span></span>
-        </a>
+    <div class="badges">
+        <div data-badge="home" class="badge">Home</div>
+        <div data-badge="overview" class="badge">Overview</div>
+        <div data-badge="about" class="badge">About</div>
+        <div data-badge="results" class="badge">Results</div>
+        <div data-badge="resources" class="badge">Resources</div>
+        <div data-badge="contact" class="badge">Contact</div>
     </div>
-    <div class="overview_nav">
-        <a href="">
-            <div class="badge">
-                Overview
-            </div>
-            <span></span>
-        </a>
-    </div>
-    <div class="about_nav">
-        <a href="">
-            <div class="badge">
-                About
-            </div>
-            <span></span>
-        </a>
-    </div>
-    <div class="results_nav">
-        <a href="">
-            <div class="badge">
-                Results
-            </div>
-            <span></span>
-        </a>
-    </div>
-    <div class="resources_nav">
-        <a href="">
-            <div class="badge">
-                Resources
-            </div>
-            <span></span>
-        </a>
-    </div>
-    <div class="contact_nav">
-        <a href="">
-            <div class="badge">
-                Contact
-            </div>
-            <span></span>
-        </a>
+    <div class="bullets">
+        <div class="home_nav">
+            <a data-badge="home" href=""></a>
+        </div>
+        <div class="overview_nav">
+          <a data-badge="home" href=""></a>
+        </div>
+        <div class="about_nav">
+            <a data-badge="home" href=""></a>
+        </div>
+        <div class="results_nav">
+            <a data-badge="results" href=""></a>
+        </div>
+        <div class="resources_nav">
+            <a data-badge="resources" href=""></a>
+        </div>
+        <div class="contact_nav">
+            <a data-badge="contact" href=""></a>
+        </div>
     </div>
 </section>
