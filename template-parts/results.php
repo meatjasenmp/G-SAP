754662b9fb167b75c2fbceb7bd7453747cc5d6c3
ntwrkjasen <jasen@thentwrk.com>
2022-09-10 16:04:18 -0500

    Results section init.

--- template-parts/results.php
+++ template-parts/results.php
@@ -1,3 +1,12 @@
+<?php
+    $results = get_group_field('content_blocks', 'g-sap_life_results', 29);
+    $results_content = $results['content'];
+?>
+
 <section class="results py-20">
+    <div class="life_results">
+        <h3><?php echo $results_content['header']; ?></h3>
+        <?php echo $results_content['content']; ?>
+    </div>
     <canvas id="credit-sufficient-students"></canvas>
 </section>
