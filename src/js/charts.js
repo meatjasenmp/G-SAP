c37e542fd247bcf099e9d408bc2c08f311f7ed8f
ntwrkjasen <jasen@thentwrk.com>
2022-09-10 16:16:01 -0500

    Results section init.

--- src/js/charts.js
+++ src/js/charts.js
@@ -109,8 +109,166 @@ const creditSufficientStudy = () => {
   }
 };
 
+const chart_one = () => {
+  const ctx = document.getElementById("chart-one").getContext("2d");
+
+  if (ctx) {
+    return new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: [
+          2016,
+          2017,
+          2018,
+          2019,
+          2020,
+          "COVID Year 2021",
+          "Post-COVID Year 2022",
+        ],
+        datasets: [
+          {
+            label: "Total Number of Students Enrolled",
+            data: [1164, 1201, 1218, 1238, 1201, 1164, 1285],
+            backgroundColor: "#0D3691",
+            order: 1,
+          },
+          {
+            label: "Total Number of Credit Sufficient Students",
+            data: [995, 1061, 1027, 1011, 1012, 819, 1086],
+            backgroundColor: "#FF8900",
+            order: 1,
+          },
+          {
+            label: "Percentage of Credit Sufficient Students",
+            data: [
+              percentage(995, 1164),
+              percentage(1061, 1201),
+              percentage(1027, 1218),
+              percentage(1011, 1238),
+              percentage(1012, 1201),
+              percentage(819, 1164),
+              percentage(1086, 1285),
+            ],
+            backgroundColor: "#A5A5A5",
+            type: "line",
+            yAxisID: "y2",
+            order: 0,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: "CREDIT SUFFICIENT STUDENTS",
+            font: {
+              family: "MagdelinRegular",
+              size: 14,
+            },
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: true,
+          },
+          y2: {
+            display: true,
+            position: "right",
+          },
+        },
+      },
+    });
+  }
+};
+
+const chart_two = () => {
+  const ctx = document.getElementById("chart-two").getContext("2d");
+
+  if (ctx) {
+    return new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: [
+          2016,
+          2017,
+          2018,
+          2019,
+          2020,
+          "COVID Year 2021",
+          "Post-COVID Year 2022",
+        ],
+        datasets: [
+          {
+            label: "Total Number of Students Enrolled",
+            data: [1164, 1201, 1218, 1238, 1201, 1164, 1285],
+            backgroundColor: "#0D3691",
+            order: 1,
+          },
+          {
+            label: "Total Number of Credit Sufficient Students",
+            data: [995, 1061, 1027, 1011, 1012, 819, 1086],
+            backgroundColor: "#FF8900",
+            order: 1,
+          },
+          {
+            label: "Percentage of Credit Sufficient Students",
+            data: [
+              percentage(995, 1164),
+              percentage(1061, 1201),
+              percentage(1027, 1218),
+              percentage(1011, 1238),
+              percentage(1012, 1201),
+              percentage(819, 1164),
+              percentage(1086, 1285),
+            ],
+            backgroundColor: "#A5A5A5",
+            type: "line",
+            yAxisID: "y2",
+            order: 0,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: "CREDIT SUFFICIENT STUDENTS",
+            font: {
+              family: "MagdelinRegular",
+              size: 14,
+            },
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: true,
+          },
+          y2: {
+            display: true,
+            position: "right",
+          },
+        },
+      },
+    });
+  }
+};
+
 const charts = () => {
   creditSufficientStudy();
+  chart_one();
+  chart_two();
 };
 
 export default charts;
--- template-parts/results.php
+++ template-parts/results.php
@@ -1,6 +1,14 @@
 <?php
     $results = get_group_field('content_blocks', 'g-sap_life_results', 29);
     $results_content = $results['content'];
+    $earned_results = $results['earned_results'];
+    $earned_results_column = $earned_results['earned_results_column'];
+    $earned_results_column_image = $earned_results_column['image'];
+    $earned_results_column_title = $earned_results_column['title'];
+    $earned_results_content_column = $earned_results['content_column'];
+    $earned_results_list = $earned_results_content_column['list'];
+    $earned_results_list_item = $earned_results_list['list_item'];
+    $earned_results_content_column_content = $earned_results_content_column['content'];
 ?>
 
 <section class="results py-20">
@@ -8,5 +16,46 @@
         <h3><?php echo $results_content['header']; ?></h3>
         <?php echo $results_content['content']; ?>
     </div>
-    <canvas id="credit-sufficient-students"></canvas>
+    <section class="charts">
+        <div>
+            <canvas id="credit-sufficient-students"></canvas>
+        </div>
+        <div class="columns">
+            <div>
+                <canvas id="chart-one"></canvas>
+            </div>
+            <div>
+                <canvas id="chart-two"></canvas>
+            </div>
+        </div>
+    </section>
+    <div class="earned_results">
+        <div class="earned_results_container">
+            <div class="wrapper">
+                <div class="column_1">
+                    <figure class="opacity-0">
+                        <img src="<?php echo $earned_results_column_image['url']; ?>" alt="<?php echo $earned_results_column_image['alt']; ?>">
+                    </figure>
+                    <h1 class="opacity-0"><?php echo $earned_results_column_title; ?></h1>
+                </div>
+                <div class="column_2">
+                    <div class="column_2_wrapper">
+                        <?php $i = 0; foreach($earned_results_list as $list): ?>
+                            <?php foreach($list as $item ): $checkbox = $item['checkbox']; $text = $item['text']; ?>
+                                <?php echo ($i % 2 === 0) ? '<div>' : '';  $i++; ?>
+                                <div class="column_2_item opacity-0">
+                                    <img src="<?php echo $checkbox['url']; ?>" alt="<?php echo $checkbox['alt']; ?>">
+                                    <span><?php echo $text; ?></span>
+                                </div>
+                                <?php echo ($i % 2 === 0) ? '</div>' : ''?>
+                            <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="column_2_text opacity-0">
+                        <?php echo strip_tags($earned_results_content_column_content, '<strong><em>'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
